6643c2c355d4c65e9a9c238051f3421ab4f10d80
VietDuc005 <ducsmile111@gmail.com>
2025-11-21 10:44:28 +0700

    update: xoa mot so file

--- app/admin/(settings)/account/page.tsx
+++
@@ -1,7 +0,0 @@
-// app/(admin)/(settings)/profile/page.tsx
-"use client";
-import AccountForm from "@/components/features/settings/AccountForm";
-
-export default function ProfilePage() {
-  return <AccountForm />;
-}
--- app/admin/(settings)/profile/page.tsx
+++
@@ -1,7 +0,0 @@
-// app/(admin)/(settings)/profile/page.tsx
-"use client";
-import ProfileForm from "@/components/features/settings/ProfileForm";
-
-export default function ProfilePage() {
-  return <ProfileForm />;
-}
